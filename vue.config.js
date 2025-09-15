73c6dee90a767ab50d445c10a9a039e649ba9fc9
yuhu <17801020645@163.com>
2025-09-15 09:30:25 +0800

    更改启动端口号为8085

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    port: 8085, // 将此处的3000改为你想要的端口号:cite[1]:cite[4]
+    // 其他devServer配置...
+  },
+});
